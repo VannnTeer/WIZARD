fd4022033bf9782e1673b1a4889517433e1f4835
VanTeer <116986372+VannnTeer@users.noreply.github.com>
2023-02-18 15:38:26 +0700

    Update quotes.ts

--- quotes.ts
+++ quotes.ts
@@ -13,53 +13,5 @@ export const Quotes: {
   // " \"He didn't die, he just escaped our eyes\" Something additional"
 }[] = [
   {
-    quote: "He will always remain in our hearts <3",
-    author: "Elliot",
-  },
-  {
-    quote: "A legend who hid his pain to make others smile. Will remain in my heart, always...",
-    author: "Ahnaf",
-  },
-  {
-    quote: "I dropkicked that child in self-defense",
-  },
-  {
-    quote: "Technoblade never dies",
-  },
-  {
-    quote: "He died but with great honor! A true hero.",
-    author: "iSoham"
-  },
-  {
-    quote: "So long, blood god",
-    author: "MSWord",
-  },
-  {
-    quote: "Forever alive in our hearts",
-    author: "synthium",
-  },
-  {
-    quote: "Long live the king ♥️",
-    author: "Vedrecide",
-  },
-  {
-    quote: "I’m happy we shared great memories together, from our duels where I got completely outclassed to the rank ceremony, events, and many more. I have a hard time watching these videos now; we didn’t know these were the good old days",
-    author: "Simon Hypixel",
-  },
-  {
-    quote: "Fly high, Technoblade. You will not be forgotten.",
-    author: "Digging_"    
-  },
-  {
-    quote: "He was truly a great man. He will be missed. See you soon buddy o7",
-    author: "hourX"    
-  },
-  {
-    quote: "I'm heartbroken that I'll never see a new technoblade video again. So long my friend, and may all your potato minions be tier 12 in heaven.",
-    author: "heurican"    
-  },
-  {
-        quote: "I am very sad i cried too",
-        author: "shabman"
   }
 ];
