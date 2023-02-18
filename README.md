f4c3abc8855dd7c28de8a107151b9e601fccaea7
VanTeer <116986372+VannnTeer@users.noreply.github.com>
2023-02-18 16:11:57 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Website-Portal - What it looks like
+
+Here is a GIF showing the site functions including: Hover Interactions, Click-To-Copy IP, Hovering Logo and Player Count!
+
+https://i.gyazo.com/08607389c89042eef20f3683b3f08c2a.gif
---
+++ js/loading.js
@@ -0,0 +1,11 @@
+/**
+ * Developed by Potters/Tara3208
+ * Not to be resold or distributed in any way
+ **/
+
+//paste this code under the head tag or in a separate js file.
+    // Wait for window load
+    $(window).load(function() {
+        // Animate loader off screen
+        $(".se-pre-con").fadeOut("slow");;
+    });
